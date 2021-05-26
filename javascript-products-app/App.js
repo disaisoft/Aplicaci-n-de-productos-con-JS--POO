4f21274bca99d393dcaaf3484db2120a9cbce434
disaisoft <disaisoft@gmail.com>
2021-05-25 22:08:17 -0500

    formulario avance

--- javascript-products-app/App.js
+++ javascript-products-app/App.js
@@ -7,8 +7,19 @@ class Product {
 }
 
 class UI {
-    addProduct(){
-
+    addProduct(product){
+       const productList = document.getElementById('product-list');
+        const element= document.createElement('div');
+        element.innerHTML = `
+            <div class="card text-center mb-4">
+                <div class="card-body">
+                    <strong>Product Name</strong>: ${product.name}
+                    <strong>Product Price</strong>: ${product.price}
+                    <strong>Product Year</strong>: ${product.year}    
+                </div>    
+            </div>
+        `;
+        productList.appendChild(element);
     }
 
     deleteProduct(){
@@ -29,6 +40,8 @@ document.getElementById('product-form')
 
     
         const product = new Product(name, price, year);
+        const ui = new UI();
+        ui.addProduct(product);
 
         e.preventDefault();
     
