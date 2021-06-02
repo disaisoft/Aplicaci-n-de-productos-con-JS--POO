1bb3d609dec6845279a49252184dc2c1e1cb3639
disaisoft <disaisoft@gmail.com>
2021-06-01 20:03:02 -0500

    actualizacio

--- javascript-products-app/App.js
+++ javascript-products-app/App.js
@@ -9,7 +9,7 @@ class Product {
 class UI {
     addProduct(product){
        const productList = document.getElementById('product-list');
-        const element= document.createElement('div');
+        const element = document.createElement('div');
         element.innerHTML = `
             <div class="card text-center mb-4">
                 <div class="card-body">
